912cbd3dcf13b9253d39140330c8da22f696f7c3
alikskonst <alikskonst@gmail.com>
2022-04-05 12:03:19 +0300

    правки регулярок

--- src/edu/kaa/romancalc/Main.java
+++ src/edu/kaa/romancalc/Main.java
@@ -1,24 +1,10 @@
 package edu.kaa.romancalc;
 
-import edu.kaa.romancalc.calc.Calc;
-import edu.kaa.romancalc.converter.Converter;
-import edu.kaa.romancalc.converter.impl.ConverterImpl;
-import edu.kaa.romancalc.line.LineCheck;
-import edu.kaa.romancalc.line.LinePreparing;
-import edu.kaa.romancalc.line.impl.LineCheckImpl;
-import edu.kaa.romancalc.line.impl.LinePreparingImpl;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        LineCheck lineCheck = new LineCheckImpl();
-        LinePreparing linePreparing = new LinePreparingImpl(lineCheck);
-        Converter converter = new ConverterImpl();
-
-        new Calc(linePreparing, lineCheck, converter).result();
-
-//        String str = "1+1+1";
-//        System.out.println(str.replaceAll("[0-9]", "").length());
+        String str = "lalala";
+        System.out.println(str);
     }
 }
